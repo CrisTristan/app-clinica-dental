d40a6c3f3cd7bd8b009e576f8e570e6602326538
Cristian <cristian.abad700@gmail.com>
2026-06-05 19:39:15 -0500

    nuevo helper para la base de datos

---
+++ app/helpers/dateTime.ts
@@ -0,0 +1,27 @@
+const pad = (value: number, size = 2) => String(value).padStart(size, "0");
+
+export const toDbTimestamp = (value: Date | string | number) => {
+  const date = new Date(value);
+
+  if (Number.isNaN(date.getTime())) {
+    throw new Error("Invalid date");
+  }
+
+  return [
+    date.getFullYear(),
+    "-",
+    pad(date.getMonth() + 1),
+    "-",
+    pad(date.getDate()),
+    "T",
+    pad(date.getHours()),
+    ":",
+    pad(date.getMinutes()),
+    ":",
+    pad(date.getSeconds()),
+    ".",
+    pad(date.getMilliseconds(), 3),
+  ].join("");
+};
+
+export const dateOnlyToDbStartOfDay = (value: string) => `${value}T00:00:00.000`;
